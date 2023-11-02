7a4d13a86c38ec9b3e1ee9bcebf8eda4afd7d447
haileyhoyat <whatuphails@yahoo.com>
2023-11-01 21:03:49 -0400

    Proof of concept: can use functions in line#27

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const book_clubs = [
 
 ]
 
-/*
+
 book_clubs.forEach(club => {
     if (club.name === "Reese Witherspoon"){
         reese()
@@ -35,60 +35,17 @@ book_clubs.forEach(club => {
         bellatrist()
     }
 });
-*/
+
 
 app.get('/', (req, res) => {
     res.json("Welcome to my celebrity book clubs api")
 })
 
 app.get('/books', (req, res)=>{
-
-    axios.get('https://reesesbookclub.com/article/4eRlfCOXueqPrm6ZnQpzwl')
-        .then((response)=>{
-            const html = response.data
-            const $ = cheerio.load(html)
-            //console.log(html)
-            $('span.css-h011xd', html).each(function(){
-                if (($(this).text() != "\"")){
-
-                    full_string = $(this).text().replace("\n", "").replace("“", "\"").replace("”", "\"").replace("’", "\'")                    
-
-                    //console.log(full_string)
-                    author = full_string.split("by")[1].trim()
-                    title = full_string.split("\"")[1].trim()
-
-                    //full date
-                    date = full_string.split("\"")[0].trim()
-                    //month
-                    month = date.split(" ")[0]
-                    //year
-                    year = "20" + date.split("\'")[1]
-                    
-                    books.push({
-                        full_string,
-                        author,
-                        title,
-                        date,
-                        month,
-                        year
-                    })
-                    
-                    //remove YA books
-                    for (let i = 0; i < books.length; i++){
-                        if (books[i].date.includes("YA")){
-                            books.pop(i)
-                        }
-                    }
-
-                }
-            })
-            res.json(books)
-        }).catch((err) => console.log(err))
-        
-        
+    res.json(books)     
 })
 
-/*
+
 function reese() {
     axios.get('https://reesesbookclub.com/article/4eRlfCOXueqPrm6ZnQpzwl')
         .then((response)=>{
@@ -129,101 +86,56 @@ function reese() {
 
                 }
             })
-            res.json(books)
+            
         }).catch((err) => console.log(err))
     }
 
     
     function jenna () {
-        axios.get('https://reesesbookclub.com/article/4eRlfCOXueqPrm6ZnQpzwl')
-        .then((response)=>{
-            const html = response.data
-            const $ = cheerio.load(html)
-            //console.log(html)
-            $('span.css-h011xd', html).each(function(){
-                if (($(this).text() != "\"")){
+        full_string = "Hello"
+        author = "Hello"
+        title = "Hello"
 
-                    full_string = $(this).text().replace("\n", "").replace("“", "\"").replace("”", "\"").replace("’", "\'")                    
-
-                    //console.log(full_string)
-                    author = "Hello"
-                    title = "Hello"
-
-                    //full date
-                    date = "Hello"
-                    //month
-                    month = "Hello"
-                    //year
-                    year = "Hello"
-                    
-                    books.push({
-                        full_string,
-                        author,
-                        title,
-                        date,
-                        month,
-                        year
-                    })
-                    
-                    //remove YA books
-                    for (let i = 0; i < books.length; i++){
-                        if (books[i].date.includes("YA")){
-                            books.pop(i)
-                        }
-                    }
-
-                }
-            })
-            res.json(books)
-        }).catch((err) => console.log(err))
+        //full date
+        date = "Hello"
+        //month
+        month = "Hello"
+        //year
+        year = "Hello"
+        
+        books.push({
+            full_string,
+            author,
+            title,
+            date,
+            month,
+            year
+        })
     }
 
     function bellatrist() {
-        axios.get('https://reesesbookclub.com/article/4eRlfCOXueqPrm6ZnQpzwl')
-        .then((response)=>{
-            const html = response.data
-            const $ = cheerio.load(html)
-            //console.log(html)
-            $('span.css-h011xd', html).each(function(){
-                if (($(this).text() != "\"")){
+        full_string = "World"
+        author = "World"
+        title = "World"
 
-                    full_string = $(this).text().replace("\n", "").replace("“", "\"").replace("”", "\"").replace("’", "\'")                    
-
-                    //console.log(full_string)
-                    author = "World"
-                    title = "World"
-
-                    //full date
-                    date = "World"
-                    //month
-                    month = "World"
-                    //year
-                    year = "World"
-                    
-                    books.push({
-                        full_string,
-                        author,
-                        title,
-                        date,
-                        month,
-                        year
-                    })
-                    
-                    //remove YA books
-                    for (let i = 0; i < books.length; i++){
-                        if (books[i].date.includes("YA")){
-                            books.pop(i)
-                        }
-                    }
-
-                }
-            })
-            res.json(books)
-        }).catch((err) => console.log(err))
-        res.json(books)
+        //full date
+        date = "World"
+        //month
+        month = "World"
+        //year
+        year = "World"
+        
+        books.push({
+            full_string,
+            author,
+            title,
+            date,
+            month,
+            year
+        })
     }
 
-*/
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
